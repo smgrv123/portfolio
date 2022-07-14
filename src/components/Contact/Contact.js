2766c6626ef0ceb82e5919a487ed34b01aeb1e08
Sumrit Grover <sumritgrover1@gmail.com>
2022-07-14 12:00:08 +0530

    validation for contact me added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,11 +1,15 @@
+import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import { contact } from "../../portfolio";
 import "./Contact.css";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   const sendEmail = (e) => {
     e.preventDefault();
-
     emailjs
       .sendForm(
         "service_wrmigmd",
@@ -15,23 +19,49 @@ const Contact = () => {
       )
       .then(
         (result) => {
-          console.log('res',result.text);
+          console.log("res", result.text);
         },
         (error) => {
           console.log(error.text);
         }
       );
-      // e.target.reset();
+    // e.target.reset();
   };
   if (!contact.email) return null;
 
   return (
     <section className="section contact center" id="contact">
       <h2 className="section__title">Contact</h2>
-      <form onSubmit={sendEmail}>
-        <input type="text" name="name" placeholder="Name" />
-        <input type="email" name="email" placeholder="Email" />
-        <textarea name="message" placeholder="Message" />
+      <form
+        onSubmit={() => {
+          if (email && name && message) {
+            sendEmail();
+          }
+        }}
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
         <button className="submitButton" type="submit">
           Send
         </button>
